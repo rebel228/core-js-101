976fb303db93c1e212a13d5bd6d1dfd504b61f99
rebel228 <dinovik@rambler.ru>
2023-05-31 01:36:48 +0300

    feat: finish objects task

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -20,8 +20,15 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  const object = {
+    width,
+    height,
+    getArea() {
+      return width * height;
+    },
+  };
+  return object;
 }
 
 
@@ -35,8 +42,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 
@@ -51,8 +58,10 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = JSON.parse(json);
+  const values = Object.values(obj);
+  return new proto.constructor(...values);
 }
 
 
@@ -110,40 +119,130 @@ function fromJSON(/* proto, json */) {
  *  For more examples see unit tests.
  */
 
+class MySuperCss {
+  constructor(el, value) {
+    this.elements = [el];
+    this.result = value;
+  }
+
+  stringify() {
+    console.log(this.result);
+    return this.result;
+  }
+
+  element(value) {
+    if (this.elements.includes('element')) {
+      throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    }
+    const wrongOrder = ['id', 'class', 'attribute', 'pseudo-class', 'pseudo-element'];
+    if (this.elements.find((item) => wrongOrder.includes(item))) {
+      throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    }
+    const resValue = `${value}`;
+    this.elements.push('element');
+    this.result += resValue;
+    return this;
+  }
+
+  id(value) {
+    if (this.elements.includes('id')) {
+      throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    }
+    const wrongOrder = ['class', 'attribute', 'pseudoClass', 'pseudoElement'];
+    if (this.elements.find((item) => wrongOrder.includes(item))) {
+      throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    }
+    const resValue = `#${value}`;
+    this.elements.push('id');
+    this.result += resValue;
+    return this;
+  }
+
+  class(value) {
+    const wrongOrder = ['attribute', 'pseudoClass', 'pseudoElement'];
+    if (this.elements.find((item) => wrongOrder.includes(item))) {
+      throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    }
+    const resValue = `.${value}`;
+    this.elements.push('class');
+    this.result += resValue;
+    return this;
+  }
+
+  attr(value) {
+    const wrongOrder = ['pseudoClass', 'pseudoElement'];
+    if (this.elements.find((item) => wrongOrder.includes(item))) {
+      throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    }
+    const resValue = `[${value}]`;
+    this.elements.push('attribute');
+    this.result += resValue;
+    return this;
+  }
+
+  pseudoClass(value) {
+    const wrongOrder = 'pseudoElement';
+    if (this.elements.includes(wrongOrder)) {
+      throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    }
+    const resValue = `:${value}`;
+    this.elements.push('pseudoClass');
+    this.result += resValue;
+    return this;
+  }
+
+  pseudoElement(value) {
+    if (this.elements.includes('pseudoElement')) {
+      throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    }
+    const resValue = `::${value}`;
+    this.elements.push('pseudoElement');
+    this.result += resValue;
+    return this;
+  }
+}
+
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  element(value) {
+    const resValue = `${value}`;
+    return new MySuperCss('element', resValue);
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  id(value) {
+    const resValue = `#${value}`;
+    return new MySuperCss('id', resValue);
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    const resValue = `.${value}`;
+    return new MySuperCss('class', resValue);
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    const resValue = `[${value}]`;
+    return new MySuperCss('attribute', resValue);
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    const resValue = `:${value}`;
+    return new MySuperCss('pseudoClass', resValue);
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    const resValue = `::${value}`;
+    return new MySuperCss('pseudoElement', resValue);
   },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
+  combine(selector1, combinator, selector2) {
+    const resValue = `${selector1.stringify()} ${combinator} ${selector2.stringify()}`;
+    return new MySuperCss('combinator', resValue);
   },
 };
-
 
 module.exports = {
   Rectangle,
   getJSON,
   fromJSON,
   cssSelectorBuilder,
+  MySuperCss,
 };
